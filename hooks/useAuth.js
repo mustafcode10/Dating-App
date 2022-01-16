f40f10819b84b286086929981f76b7ae523c2e8c
Mustaf <mustafdiiriye@hotmail.com>
2022-01-16 17:28:11 +0200

    google signIn

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -4,24 +4,30 @@ import * as Google from "expo-google-app-auth"
 const AuthContext = createContext({});
 
 const config = {
-    androidClientId: '278485296468-55d4cvdil0edsbi39ln6omnu3dudu24s.apps.googleusercontent.com',
+    androidClientId: '1036634927114-v4tc5q5f0bspqjqhu9tql7vb3ceb763j.apps.googleusercontent.com',
+    iosClientId: '1036634927114-1go12bmc0ccivic8neatt10t8qofl6r8.apps.googleusercontent.com',
     scopes: ["profile", "email" ],
     permissions:["public_profile", "email", "gender", "location"]
 }
 
 export const AuthProvider = ({ children }) => {
     const signInWithGoogle = async () =>{
+      console.log('GOOGLE', Google)
+        console.log('CONFIG', config)
         Google.logInAsync(config).then(async (logInResult) => {
            if(logInResult.type === "success"){
-           //...........
+           console.log(logInResult)
            }
         })
     }
+
+ 
+
   return (
     <AuthContext.Provider
       value={{
         user: null,
-        signInWithGoogle: signInWithGoogle,
+        signInWithGoogle,
       }}
     >
       {children}
