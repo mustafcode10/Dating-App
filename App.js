0689f5ba608b4fe0fbf7109066f599c3969b6302
Mustaf <mustafdiiriye@hotmail.com>
2022-01-17 13:29:40 +0200

    google credential login with firebase

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from "react";
 import StackNavigator from "./StackNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from './hooks/useAuth';
+import { LogBox } from "react-native"
+LogBox.ignoreAllLogs() // ignore all log messages
 
 export default function App() {
   return (
--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,27 +1,37 @@
 import React, { createContext, useContext } from "react";
-import * as Google from "expo-google-app-auth"
+import * as Google from "expo-google-app-auth";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithCredential,
+  signOut
+} from "firebase/auth";
+
+import { auth } from "./../firebase";
 
 const AuthContext = createContext({});
 
 const config = {
-    androidClientId: '1036634927114-aie1h7064sbu2cni4s31p22fu0sm9qji.apps.googleusercontent.com',
-    iosClientId: '1036634927114-1go12bmc0ccivic8neatt10t8qofl6r8.apps.googleusercontent.com',
-    scopes: ["profile", "email" ],
-    permissions:["public_profile", "email", "gender", "location"]
-}
+  androidClientId:
+    "1036634927114-aie1h7064sbu2cni4s31p22fu0sm9qji.apps.googleusercontent.com",
+  iosClientId:
+    "1036634927114-1go12bmc0ccivic8neatt10t8qofl6r8.apps.googleusercontent.com",
+  scopes: ["profile", "email"],
+  permissions: ["public_profile", "email", "gender", "location"],
+};
 
 export const AuthProvider = ({ children }) => {
-    const signInWithGoogle = async () =>{
-      console.log('GOOGLE', Google)
-        console.log('CONFIG', config)
-        Google.logInAsync(config).then(async (logInResult) => {
-           if(logInResult.type === "success"){
-           console.log(logInResult)
-           }
-        })
-    }
-
- 
+  const signInWithGoogle = async () => {
+    await Google.logInAsync(config).then(async (logInResult) => {
+      if (logInResult.type === "success") {
+        // login ..........
+        const { idToken, accessToken } = logInResult;
+        const credential = GoogleAuthProvider.credential(idToken, accessToken);
+        await signInWithCredential(auth, credential);
+      }
+      return Promise.reject();
+    });
+  };
 
   return (
     <AuthContext.Provider
