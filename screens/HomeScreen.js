bc0ab599507c5aebc9f87c544ea3a7c2a0ba4c08
Mustaf <mustafdiiriye@hotmail.com>
2022-01-22 17:39:06 +0200

    finished card swiper style

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -17,26 +17,38 @@ const DUMMY_DATA = [
   {
     id: 1,
     firstName: "Mustaf",
+    lastName: "Dirie",
     photoURL:
-      "https://scontent.famm2-3.fna.fbcdn.net/v/t1.6435-1/s320x320/251500216_4577582788951742_7047083087247365839_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=7206a8&_nc_eui2=AeGkf7X2j2-vKuZwq-fSyTdPUTl1NS-80atROXU1L7zRq1qshKi4YqUry0pfKg8rlnQdP7eDVdnt51S9sbLEdkZc&_nc_ohc=hvHpfSrPrIIAX_kD2Nq&_nc_ht=scontent.famm2-3.fna&oh=ae311833ddd85cc826f55b04323a849f&oe=61AD14EF",
+      "https://instagram.famm2-3.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/250927798_470183747745566_2681257997982768721_n.jpg?_nc_ht=instagram.famm2-3.fna.fbcdn.net&_nc_cat=102&_nc_ohc=Xio8GVfcza0AX9PSBEZ&edm=ALQROFkBAAAA&ccb=7-4&ig_cache_key=MjY5NzIwNjQ2MTE3MzQwMjI5NA%3D%3D.2-ccb7-4&oh=00_AT_vupzi4OwztHLgmS_8Ri9liz00UpqDO78NBm9ierRWZQ&oe=61F20D42&_nc_sid=30a2ef",
+    age: 26,
+    job: "Software Developer",
   },
   {
     id: 2,
-    firstName: "Cinur",
+    firstName: "Meryem ",
+    lastName: "Uzerli",
     photoURL:
-      "https://scontent.famm2-3.fna.fbcdn.net/v/t31.18172-8/15937157_1606808016012543_2337087566501078422_o.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeF9UKT_6pw0WdYFOjzbFSFznxWHCzeAEJ2fFYcLN4AQnReWBTQTeaGC9Aoe5pjiFZJbQxuKliq4R8-FHQMzX5ep&_nc_ohc=vu3V2pYiL4cAX_15QmA&_nc_ht=scontent.famm2-3.fna&oh=925d3f6c1746191fc163516de6f58683&oe=61B01875",
-  },
-  {
-    id: 7,
-    firstName: "Sumi Qurox",
-    photoURL:
-      "https://scontent.famm2-3.fna.fbcdn.net/v/t1.6435-1/p320x320/101547911_1354523758271738_1676447283203276800_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=7206a8&_nc_eui2=AeEF8Ool3z8QmerFeCziCPP4L0TtgokhKB8vRO2CiSEoH9VZi0tde2Gf1-31XjBuHaEjdCEv6LGnTnywxy9GEVnE&_nc_ohc=Jz0239DgT9gAX8eecgD&tn=fZvDsFi2DvSZ3hrK&_nc_ht=scontent.famm2-3.fna&oh=a46a6b65b1e0c6749b3c9b9965e57671&oe=61B8DF80",
+      "https://cdn.technosports.co.in/wp-content/uploads/2021/11/Meryem.jpg",
+    age: 38 ,
+    job: "Actress and Model",
   },
   {
     id: 3,
-    firstName: "Ismael_Mohamed",
+    firstName: "Duckie",
+    lastName: "Thot",
+    photoURL:
+      "https://cdn.technosports.co.in/wp-content/uploads/2021/11/Duckie-Thot-1064x1536.jpg",
+    age:  26,
+    job: "Model",
+  },
+  {
+    id: 4,
+    firstName: "Ismael",
+    lastName: "Mohamed",
     photoURL:
       "https://scontent.famm2-3.fna.fbcdn.net/v/t1.6435-9/166765193_4219191584760278_5419838317690091680_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGnrrAyG0tXSaLxoJMpkeB-1O1kY0LZnCrU7WRjQtmcKglaSPUl4q1_hkf0M9296S0M7WMlJNR5yNdTuNeqNWvf&_nc_ohc=sbJzuP-7EdEAX92cYzb&_nc_ht=scontent.famm2-3.fna&oh=19c16b2e9d3d5fc0fbb6505b9e362659&oe=61ABC46B",
+    age: 26,
+    job: "self-employed",
   },
 ];
 
@@ -65,18 +77,83 @@ const HomeScreen = ({ navigation }) => {
       </View>
       {/* End of header */}
       {/* Cards */}
-      <View style={{flex: 1, marginTop: 10,}}>
+      <View style={{ flex: 1, marginTop: 10 }}>
         <Swiper
-        containerStyle={{backgroundColor: 'transparent'}}
+          containerStyle={{ backgroundColor: "transparent" }}
           cards={DUMMY_DATA}
           stackSize={5}
           cardIndex={0}
           animateCardOpacity
           verticalSwipe={false}
+          overlayLabels={{
+            left: {
+              title: "NOPE",
+              style: {
+                label: {
+                  textAlign: "right",
+                  color: "red",
+                },
+              },
+            },
+            right: {
+              title: "MATCH",
+              style: {
+                label: {
+                  color: "#4DED30",
+                },
+              },
+            },
+          }}
           renderCard={(card) => (
-            <View key={card.id} style={{backgroundColor: 'white', height: 500, borderRadius: 10}}>
+            <View
+              key={card.id}
+              style={{
+                position: "relative",
+                backgroundColor: "white",
+                height: 500,
+                borderRadius: 10,
+              }}
+            >
               {/* <Text style={{fontSize: 18, backgroundColor: 'grey'}}>{card.firstName}</Text> */}
-              <Image style={{ height:500, borderRadius: 10}} source={{uri: card.photoURL}} />
+              <Image
+                style={{ height: 500, borderRadius: 10 }}
+                source={{ uri: card.photoURL }}
+              />
+              <View
+                style={{
+                  backgroundColor: "white",
+                  borderRadius: 10,
+                  height: 70,
+                  marginTop: 10,
+                  position: "absolute",
+                  width: 371.5,
+                  bottom: 0,
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    margin: 15,
+                    shadowColor: "#000",
+                    shadowOffset: {
+                      width: 0,
+                      height: 1,
+                    },
+                    shadowOpacity: 0.2,
+                    shadowRadius: 1.41,
+                    elevation: 2,
+                  }}
+                >
+                  <View>
+                    <Text style={{ fontWeight: "bold" }}>
+                      {card.firstName} {card.lastName}
+                    </Text>
+                    <Text>{card.job}</Text>
+                  </View>
+                  <Text style={{ fontWeight: "bold" }}>{card.age}</Text>
+                </View>
+              </View>
             </View>
           )}
         />
